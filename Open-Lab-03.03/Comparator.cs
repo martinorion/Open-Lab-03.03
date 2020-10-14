38aa2a188dbdb7df8dd7c344cc740932bc29a12c
martinorion <71751917+martinorion@users.noreply.github.com>
2020-10-14 09:53:49 +0200

    if str1.Lenght == str2.Length return true, else return false

--- Open-Lab-03.03/Comparator.cs
+++ Open-Lab-03.03/Comparator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._03
 {
@@ -6,7 +6,14 @@ namespace Open_Lab_03._03
     {
         public bool CompareCharactersCount(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length == str2.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
